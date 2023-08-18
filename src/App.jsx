bb2dd0b259506ca516cb80ff27015fa9fdc083a3
Aditya Thakur <adityathakurxd@gmail.com>
2023-08-18 11:43:59 +0530

    Working hard coded poll example

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import React, { useEffect } from "react";
 import JoinForm from "./JoinForm";
 import Conference from "./Conference";
+import { Button } from "@100mslive/roomkit-react";
 
 export default function App() {
   const isConnected = useHMSStore(selectIsConnectedToRoom);
@@ -21,12 +22,69 @@ export default function App() {
     };
   }, [hmsActions, isConnected]);
 
+  const handleCreate = async (id) => {
+    console.log("POLL CREATED with ${id}");
+    await hmsActions.interactivityCenter.addQuestionsToPoll(id, [
+      {
+        text: "Vote for your favourite creator",
+        type: "single-choice",
+        options: [
+          {
+            text: "Aditya ",
+            isCorrectAnswer: false,
+          },
+          {
+            text: "Thakur ",
+            isCorrectAnswer: false,
+          },
+        ],
+        skippable: true,
+      },
+      // {
+      //   text: "Who is u?",
+      //   type: "single-choice",
+      //   options: [
+      //     {
+      //       text: "u ",
+      //       isCorrectAnswer: false,
+      //     },
+      //     {
+      //       text: "me",
+      //       isCorrectAnswer: false,
+      //     },
+      //   ],
+      //   skippable: true,
+      // },
+    ]);
+    await hmsActions.interactivityCenter.startPoll(id);
+  };
+
   return (
     <div className="App">
       {isConnected ? (
         <>
           <Conference />
           <Footer />
+          <Button
+            variant="primary"
+            css={{ mt: "$10" }}
+            onClick={async () => {
+              const id = Date.now().toString();
+              await hmsActions.interactivityCenter
+                .createPoll({
+                  id,
+                  title: "Big Boss Inspired Poll",
+                  type: "poll",
+
+                  rolesThatCanViewResponses: ["host"],
+                })
+                .then(() => handleCreate(id))
+                .catch((err) => console.log(err.message));
+            }}
+          >
+            Create
+          </Button>
+          {/* <Voting toggleVoting={toggleWidget} id={pollID} /> */}
         </>
       ) : (
         <JoinForm />
